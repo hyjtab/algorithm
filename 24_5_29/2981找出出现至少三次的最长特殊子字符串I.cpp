fe4b8b7227fc85c7d414633488341bcf842ab63e
hyjtab <2741521652@qq.com>
2024-05-29 18:24:31 +0800

    2981. 找出出现至少三次的最长特殊子字符串 I

---
+++ 24_5_29/2981找出出现至少三次的最长特殊子字符串I.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maximumLength(string s) {
+        int res = -1;
+        int curlen = 1;//当前子串长度
+        int curc = s[0];
+        for(int i = 0; i < s.length() - 1; i++){
+            int cnt = 1;//子串出现次数
+            for(int j = i; j < s.length() - 1; j++){
+                bool flag = true;
+                int upper = -(curlen - 2);//长度为一的初值不同。
+                if(curlen == 1) upper = 1; 
+                for(int k = upper; k <= 1; k++){
+                    if(s[j + k] != curc){
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag){
+                    cnt++;
+                }
+                if(cnt >= 3) break;
+            }
+            if(cnt >= 3 && res < curlen){
+                res = curlen;
+            }
+
+            if(s[i] == s[i + 1]){
+                curlen++; 
+            }else{
+                curlen = 1;
+                curc = s[i + 1];
+            }
+        }
+        return res;
+    }
+};
